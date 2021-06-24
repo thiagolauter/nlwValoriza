be96d1629bcf69bf52dd3b1c22bf6ae57a05dead
Thiago Lauter <thiagosantoslauter@gmail.com>
2021-06-24 02:00:50 -0300

    day 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
-import { request, response, Router } from "express";
+import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
-const createUserController = new CreateUserController();
 
-router.post("/users", createUserController.handle);
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+
+router.post("/users", ensureAdmin, createUserController.handle);
+router.post("/tags", createTagController.handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import "./database";
 import { router } from "./routes";
@@ -7,4 +8,16 @@ const app = express();
 
 app.use(express.json())
 app.use(router);
-app.listen(3000, () => console.log("Server running in: http://localhost:3000"));
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message,
+        });
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Iternal Server Error",
+    });
+}
+);
+app.listen(3000, () => console.log("Server running at: http://localhost:3000"));
