d9d74d65263594a096bd6f6852c97fa37a1c9c4d
Thiago Lauter <thiagosantoslauter@gmail.com>
2021-06-25 00:25:34 -0300

    Day 4

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
-import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", ensureAdmin, createUserController.handle);
 router.post("/tags", createTagController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle)
 
 export { router };
